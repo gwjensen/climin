67941e2c079561393e4a019aca9bba88738c0e7f
Grady <grady.jensen@brml.org>
2016-03-15 17:49:32 +0100

    Removed the Ctrl-c workaround after the Anaconda 2.5.0 upgrade because it was causing problems.

--- climin/__init__.py
+++ climin/__init__.py
@@ -3,15 +3,15 @@ from __future__ import absolute_import
 # What follows is part of a hack to make control breaking work on windows even
 # if scipy.stats ims imported. See:
 # http://stackoverflow.com/questions/15457786/ctrl-c-crashes-python-after-importing-scipy-stats
-import sys
-import os
-import imp
-import ctypes
-
-if sys.platform == 'win32':
-    basepath = imp.find_module('numpy')[1]
-    ctypes.CDLL(os.path.join(basepath, 'core', 'libmmd.dll'))
-    ctypes.CDLL(os.path.join(basepath, 'core', 'libifcoremd.dll'))
+#import sys
+#import os
+#import imp
+#import ctypes
+print "\n\n\n****CLIMIN*************************************************************************************************************\n\n\n\n"
+#if sys.platform == 'win32':
+#    basepath = imp.find_module('numpy')[1]
+#    ctypes.CDLL(os.path.join(basepath, 'core', 'libmmd.dll'))
+#    ctypes.CDLL(os.path.join(basepath, 'core', 'libifcoremd.dll'))
 
 from .adadelta import Adadelta
 from .adam import Adam
